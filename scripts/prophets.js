c42dda7fa817843d64bc71685d569803a47371cc
faharetana-rkt <rakotomaharitra@gmail.com>
2025-03-13 20:57:36 +0300

    working prophet fetch, styling left

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,10 +1,28 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
 
-async function getProphet(url) {
+async function getProphets() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
-getProphet();
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        fullName.innerHTML = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', `${prophet.imageurl}`);
+        portrait.setAttribute('alt', `Portrait image of ${fullName}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
+
+getProphets();
