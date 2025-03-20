707b7a756d4cebd83a326e172211228c5f48e3d1
faharetana-rkt <rakotomaharitra@gmail.com>
2025-03-20 18:09:33 +0300

    began directory page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,28 @@
+// Date rendering
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+document.getElementById("currentYear").innerHTML = `&copy; ${year}`;
+
+// Last modified rendering
+const lastModified = document.lastModified;
+document.querySelector(".lastModified").innerHTML = `Last modified on: ${lastModified}`;
+
+/*hamburger button*/
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("#animateme");
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+// Detecting active page
+document.addEventListener("DOMContentLoaded", function () {
+	const links = document.querySelectorAll(".link");
+	const currentUrl = window.location.pathname.split("/").pop();
+  
+	links.forEach(link => {
+	  if (link.getAttribute("href") === currentUrl) {
+		link.classList.add("active");
+	  }
+	});
+  });
