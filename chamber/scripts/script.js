6c146dfc0d7d81422b1a5391654c14e2130c9f4a
faharetana-rkt <rakotomaharitra@gmail.com>
2025-03-21 10:33:00 +0300

    began script for directory

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,28 +1,38 @@
-// Date rendering
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-document.getElementById("currentYear").innerHTML = `&copy; ${year}`;
+// // Date rendering
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
+// document.getElementById("currentYear").innerHTML = `&copy; ${year}`;
 
-// Last modified rendering
-const lastModified = document.lastModified;
-document.querySelector(".lastModified").innerHTML = `Last modified on: ${lastModified}`;
+// // Last modified rendering
+// const lastModified = document.lastModified;
+// document.querySelector(".lastModified").innerHTML = `Last modified on: ${lastModified}`;
 
-/*hamburger button*/
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector("#animateme");
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+// /*hamburger button*/
+// const hamButton = document.querySelector("#menu");
+// const navigation = document.querySelector("#animateme");
+// hamButton.addEventListener('click', () => {
+// 	navigation.classList.toggle('open');
+// 	hamButton.classList.toggle('open');
+// });
 
-// Detecting active page
-document.addEventListener("DOMContentLoaded", function () {
-	const links = document.querySelectorAll(".link");
-	const currentUrl = window.location.pathname.split("/").pop();
+// // Detecting active page
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const links = document.querySelectorAll(".link");
+// 	const currentUrl = window.location.pathname.split("/").pop();
   
-	links.forEach(link => {
-	  if (link.getAttribute("href") === currentUrl) {
-		link.classList.add("active");
-	  }
-	});
-  });
+// 	links.forEach(link => {
+// 	  if (link.getAttribute("href") === currentUrl) {
+// 		link.classList.add("active");
+// 	  }
+// 	});
+//   });
+
+//Rendering businesses
+const url = "https://faharetana-rkt.github.io/wdd230/chamber/data/members.json";
+async function getMembers() {
+	const response = await fetch(url);
+	const data = await response.json();
+	console.table(data.members);
+}
+
+getMembers();
