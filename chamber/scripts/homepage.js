a0c0ec248ddc371b006b78ca0a8e07ccfc75504e
faharetana-rkt <rakotomaharitra@gmail.com>
2025-08-16 15:00:30 +0300

    finished chamber homepage

--- chamber/scripts/homepage.js
+++ chamber/scripts/homepage.js
@@ -5,69 +5,88 @@ document.getElementById("currentYear").innerHTML = `&copy; ${year}`;
 
 // Last modified rendering
 const lastModified = document.lastModified;
-document.querySelector("#lastModified").innerHTML = `Last modified on: ${lastModified}`;
+document.querySelector(
+  "#lastModified"
+).innerHTML = `Last modified on: ${lastModified}`;
 
 /*hamburger button*/
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector("#animateme");
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
 
 // Detecting active page
 document.addEventListener("DOMContentLoaded", function () {
-	const links = document.querySelectorAll(".link");
-	const currentUrl = window.location.pathname.split("/").pop();
-  
-	links.forEach(link => {
-	  if (link.getAttribute("href") === currentUrl) {
-		link.classList.add("active");
-	  }
-	});
+  const links = document.querySelectorAll(".link");
+  const currentUrl = window.location.pathname.split("/").pop();
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === currentUrl) {
+      link.classList.add("active");
+    }
+  });
+});
+
+//Rendering random businesses
+const urlMember =
+  "https://faharetana-rkt.github.io/wdd231/chamber/data/members.json";
+const container = document.querySelector(".random-businesses");
+
+function randomItemArray(array) {
+  if (array.length === 0) return null; // nothing left to pick
+  const index = Math.floor(Math.random() * array.length);
+  return array.splice(index, 1)[0]; // remove and return item
+}
+
+async function getMembersData() {
+  const response = await fetch(urlMember);
+  const data = await response.json();
+  const silverGoldArray = [];
+  data.forEach((member) => {
+    if (member.membership == "gold" || member.membership == "silver") {
+      silverGoldArray.push(member);
+    }
   });
 
-// //Rendering businesses
-// const url = "https://faharetana-rkt.github.io/wdd231/chamber/data/members.json";
-// const container = document.querySelector(".businesses");
+  displayMember(randomItemArray(silverGoldArray));
+  displayMember(randomItemArray(silverGoldArray));
+  displayMember(randomItemArray(silverGoldArray));
+}
 
-// async function getMembersData() {
-// 	const response = await fetch(url);
-// 	const data = await response.json();
-// 	displayMember(data);
-// }
+const displayMember = (member) => {
+  const card = document.createElement("div");
+  const logo = document.createElement("img");
+  const name = document.createElement("h3");
+  const address = document.createElement("p");
+  const contact = document.createElement("p");
+  const website = document.createElement("p");
+  const containerText = document.createElement("div");
 
-// const displayMember = (members) => {
-//     members.forEach((member) => {
-//         const card = document.createElement("div");
-//         const logo = document.createElement("img");
-//         const name = document.createElement("h3");
-//         const address = document.createElement("p");
-//         const contact = document.createElement("p");
-//         const website = document.createElement("p");
+  logo.src = member.image;
+  logo.alt = `${member.name} logo`;
+  logo.setAttribute("width", "150");
+  logo.setAttribute("height", "auto");
+  containerText.setAttribute("class", "text-field");
+  card.setAttribute("class", "randoms");
 
-//         logo.src = member.image;
-//         logo.alt = `${member.name} logo`;
-//         logo.setAttribute("width", "150");
-//         logo.setAttribute("height", "auto");
-//         name.innerText = member.name;
-//         address.innerText = member.address;
-//         contact.innerText = member.phone;
-//         website.innerText = member.site;
+  name.innerText = member.name;
+  contact.innerHTML = `<span class="bold-underline">Tel:</span> ${member.phone}`;
+  website.innerHTML = `<span class="bold-underline">URL:</span> ${member.site}`;
+  address.innerHTML = `<span class="bold-underline">Address:</span> ${member.address}`;
 
-//         card.appendChild(logo);
-//         card.appendChild(name);
-//         card.appendChild(address);
-//         card.appendChild(contact);
-//         card.appendChild(website);
+  card.appendChild(name);
+  card.appendChild(logo);
+  containerText.appendChild(address)
+  containerText.appendChild(contact);
+  containerText.appendChild(website);
+  card.appendChild(containerText);
 
+  container.appendChild(card);
+};
 
-//         container.appendChild(card);
-//     });
-// }
-
-// getMembersData();
-
+getMembersData();
 
 /*Current Weather api -18.149582216322468, 49.40376319374992*/
 const key = "f26976f1030d69a9f418448c9d6ec7d5";
@@ -82,21 +101,24 @@ const sunset = document.querySelector("#sunset");
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=-18.15&lon=49.4&appid=${key}&units=metric`;
 
 async function currentWeatherFetch() {
-  try{    
-      const response = await fetch(url);
-      if (response.ok) {
-          data = await response.json();
-          displayWeather(data);
-      } else {
-          throw Error(await response.text());
-      }
-  } catch(error) {
-      console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      data = await response.json();
+      displayWeather(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
   }
-};
+}
 
 function capitalizeWords(string) {
-    return string.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+  return string
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 const displayWeather = (data) => {
@@ -127,44 +149,60 @@ currentWeatherFetch();
 const first = document.querySelector("#day1");
 const second = document.querySelector("#day2");
 const third = document.querySelector("#day3");
-const days = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=-18.15&lon=49.4&appid=${key}&units=metric`;
 
 async function weatherForecastFetch() {
-    try{    
-        const response = await fetch(forecastUrl);
-        if (response.ok) {
-            data = await response.json();
-            displayForecast(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch(error) {
-        console.log(error);
+  try {
+    const response = await fetch(forecastUrl);
+    if (response.ok) {
+      data = await response.json();
+      displayForecast(data);
+    } else {
+      throw Error(await response.text());
     }
-  };
-
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const displayForecast = (data) => {
-    const day1 = new Date(data.list[0].dt * 1000);
-    first.innerHTML = `${days[day1.getDay()]}: ${Math.round(data.list[0].main.temp)}°C`;
+  const day1 = new Date(data.list[0].dt * 1000);
+  first.innerHTML = `<span class="bold-underline">Today:</span> ${Math.round(
+    data.list[0].main.temp
+  )}°C`;
 
-    //Find date 24h and 48h from day1
-    const secondData = data.list.find(entry => {
-        let entryDate = new Date(entry.dt * 1000);
-        return entryDate.getDate() !== day1.getDate();
-    });
+  //Find date 24h and 48h from day1
+  const secondData = data.list.find((entry) => {
+    let entryDate = new Date(entry.dt * 1000);
+    return entryDate.getDate() !== day1.getDate();
+  });
 
-    const thirdData = data.list.find(entry => {
-        let entryDate = new Date(entry.dt * 1000);
-        return entryDate.getDate() !== day1.getDate() && entryDate.getDate() !== new Date(secondData.dt * 1000).getDate();
-    });
+  const thirdData = data.list.find((entry) => {
+    let entryDate = new Date(entry.dt * 1000);
+    return (
+      entryDate.getDate() !== day1.getDate() &&
+      entryDate.getDate() !== new Date(secondData.dt * 1000).getDate()
+    );
+  });
 
-    const day2 = new Date(secondData.dt * 1000);
-    const day3 = new Date(thirdData.dt * 1000);
-    second.innerHTML = `${days[day2.getDay()]}: ${Math.round(secondData.main.temp)}°C`;
-    third.innerHTML = `${days[day3.getDay()]}: ${Math.round(thirdData.main.temp)}°C`;
-}
+  const day2 = new Date(secondData.dt * 1000);
+  const day3 = new Date(thirdData.dt * 1000);
+  second.innerHTML = `<span class="bold-underline">${
+    days[day2.getDay()]
+  }:</span> ${Math.round(secondData.main.temp)}°C`;
+  third.innerHTML = `<span class="bold-underline">${
+    days[day3.getDay()]
+  }:</span> ${Math.round(thirdData.main.temp)}°C`;
+};
 
 weatherForecastFetch();
--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,66 +5,67 @@ document.getElementById("currentYear").innerHTML = `&copy; ${year}`;
 
 // Last modified rendering
 const lastModified = document.lastModified;
-document.querySelector("#lastModified").innerHTML = `Last modified on: ${lastModified}`;
+document.querySelector(
+  "#lastModified"
+).innerHTML = `Last modified on: ${lastModified}`;
 
 /*hamburger button*/
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector("#animateme");
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
 
 // Detecting active page
 document.addEventListener("DOMContentLoaded", function () {
-	const links = document.querySelectorAll(".link");
-	const currentUrl = window.location.pathname.split("/").pop();
-  
-	links.forEach(link => {
-	  if (link.getAttribute("href") === currentUrl) {
-		link.classList.add("active");
-	  }
-	});
+  const links = document.querySelectorAll(".link");
+  const currentUrl = window.location.pathname.split("/").pop();
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === currentUrl) {
+      link.classList.add("active");
+    }
   });
+});
 
 //Rendering businesses
 const url = "https://faharetana-rkt.github.io/wdd231/chamber/data/members.json";
 const container = document.querySelector(".businesses");
 
 async function getMembersData() {
-	const response = await fetch(url);
-	const data = await response.json();
-	displayMember(data);
+  const response = await fetch(url);
+  const data = await response.json();
+  displayMember(data);
 }
 
 const displayMember = (members) => {
-    members.forEach((member) => {
-        const card = document.createElement("div");
-        const logo = document.createElement("img");
-        const name = document.createElement("h3");
-        const address = document.createElement("p");
-        const contact = document.createElement("p");
-        const website = document.createElement("p");
+  members.forEach((member) => {
+    const card = document.createElement("div");
+    const logo = document.createElement("img");
+    const name = document.createElement("h3");
+    const address = document.createElement("p");
+    const contact = document.createElement("p");
+    const website = document.createElement("p");
 
-        logo.src = member.image;
-        logo.alt = `${member.name} logo`;
-        logo.setAttribute("width", "150");
-        logo.setAttribute("height", "auto");
-        name.innerText = member.name;
-        address.innerText = member.address;
-        contact.innerText = member.phone;
-        website.innerText = member.site;
+    logo.src = member.image;
+    logo.alt = `${member.name} logo`;
+    logo.setAttribute("width", "150");
+    logo.setAttribute("height", "auto");
+    name.innerText = member.name;
+    address.innerText = member.address;
+    contact.innerText = member.phone;
+    website.innerText = member.site;
 
-        card.appendChild(logo);
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(contact);
-        card.appendChild(website);
+    card.appendChild(logo);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(contact);
+    card.appendChild(website);
 
-
-        container.appendChild(card);
-    });
-}
+    container.appendChild(card);
+  });
+};
 
 getMembersData();
 
@@ -75,12 +76,11 @@ const listbutton = document.querySelector("#list");
 const display = document.querySelector(".businesses");
 
 gridbutton.addEventListener("click", () => {
-    display.classList.add("grid");
-    display.classList.remove("list");
+  display.classList.add("grid");
+  display.classList.remove("list");
 });
 
 listbutton.addEventListener("click", () => {
-    display.classList.add("list");
-    display.classList.remove("grid");
-})
-
+  display.classList.add("list");
+  display.classList.remove("grid");
+});
