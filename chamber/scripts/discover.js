816e25ebf4b99e0344f21a5baf80fe83f41a7dbf
faharetana-rkt <rakotomaharitra@gmail.com>
2025-08-25 18:13:53 +0300

    finished js of discover, styling left

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,37 @@
+const url = "https://faharetana-rkt.github.io/wdd231/chamber/data/places.json";
+const cardContainer = document.querySelector("#discover-cards");
+
+async function getPlaces() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    //console.log(data);
+    displayPlaces(data);
+  } catch (error) {
+    console.error("Fetch error", error);
+  }
+};
+
+getPlaces();
+
+function displayPlaces(places) {
+    places.forEach(place => {
+        const div = document.createElement("div");
+        div.setAttribute("class", "card-place");
+        div.innerHTML = `
+        <h2>${place.name}</h2>
+        <figure>
+            <img src="${place.link}" alt="an image of ${place.name}">
+            <figcaption>Image of ${place.name}</figcaption>
+        </figure>
+        <p>${place.description}</p>
+        <a href="${place.website}">
+            <button>Learn more</button>
+        </a>
+        `;
+        cardContainer.appendChild(div);
+    });
+};
